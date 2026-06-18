89fcef8def644f373e5d7a95dd15e5b03c3cb503
Tran Duy <161484296+duynohope@users.noreply.github.com>
2026-06-18 19:33:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const particles =
+document.getElementById("particles");
+
+for(let i=0;i<60;i++){
+
+    const p =
+    document.createElement("div");
+
+    p.classList.add("particle");
+
+    const size =
+    Math.random()*4+1;
+
+    p.style.width =
+    size+"px";
+
+    p.style.height =
+    size+"px";
+
+    p.style.left =
+    Math.random()*100+"%";
+
+    p.style.top =
+    Math.random()*100+"%";
+
+    p.style.opacity =
+    Math.random()*0.7+0.1;
+
+    p.style.animationDuration =
+    Math.random()*20+15+"s";
+
+    p.style.animationDelay =
+    Math.random()*10+"s";
+
+    particles.appendChild(p);
+}
 const btn = document.getElementById("openBtn");
 
 const welcome = document.getElementById("welcome-screen");
